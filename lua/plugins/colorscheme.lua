3aad79a693d444f2e110fc71b55986b212d5e643
Madyson Bradford <89798314+MadysonBradford@users.noreply.github.com>
2024-04-07 14:09:42 -0700

    add nvim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  'comfysage/evergarden',
+  opts = {
+    transparent_background = true,
+    contrast_dark = 'medium', -- 'hard'|'medium'|'soft'
+    overrides = { }, -- add custom overrides
+  }
+}
